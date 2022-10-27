35b531ef88c84d02cfb6191bbf1e43ce93db2773
LaBrina Loving <labrina.loving@gmail.com>
2022-10-27 11:01:59 -0400

    Add project files.

---
+++ wpf-msal/App.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Identity.Client;
+using Microsoft.Identity.Client.Extensions.Msal;
+
+namespace wpf_msal
+{
+    
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static string ClientId = ""; //put in config - client id from app registration
+
+        private static string Tenant = ""; //put in config - tenant id or common or organization
+
+        private static IPublicClientApplication _clientApp;
+
+        public static IPublicClientApplication PublicClientApp { get { return _clientApp; } }
+
+        static App()
+        {
+            _clientApp = PublicClientApplicationBuilder.Create(ClientId)
+                .WithAuthority(AzureCloudInstance.AzurePublic, Tenant)
+                .WithDefaultRedirectUri()
+                .Build();
+            Task.Run(() => CrossPlatformTokenCache());
+        }
+        private static async Task CrossPlatformTokenCache()
+        {
+            // Building StorageCreationProperties
+            var storageProperties =
+                 new StorageCreationPropertiesBuilder(CacheSettings.CacheFileName, CacheSettings.CacheDir, ClientId)
+                 .WithLinuxKeyring(
+                     CacheSettings.LinuxKeyRingSchema,
+                     CacheSettings.LinuxKeyRingCollection,
+                     CacheSettings.LinuxKeyRingLabel,
+                     CacheSettings.LinuxKeyRingAttr1,
+                     CacheSettings.LinuxKeyRingAttr2)
+                 .WithMacKeyChain(
+                     CacheSettings.KeyChainServiceName,
+                     CacheSettings.KeyChainAccountName)
+                 .Build();
+
+            // This hooks up the cross-platform cache into MSAL
+            var cacheHelper = await MsalCacheHelper.CreateAsync(storageProperties);
+            cacheHelper.RegisterCache(_clientApp.UserTokenCache);
+        }
+
+
+
+        // Below are the clientId (Application Id) of your app registration and the tenant information.
+        // You have to replace:
+        // - the content of ClientID with the Application Id for your app registration
+        // - the content of Tenant by the information about the accounts allowed to sign-in in your application:
+        //   - For Work or School account in your org, use your tenant ID, or domain
+        //   - for any Work or School accounts, use `organizations`
+        //   - for any Work or School accounts, or Microsoft personal account, use `common`
+        //   - for Microsoft Personal account, use consumers
+        
+    }
+
+  
+}
+
---
+++ wpf-msal/MainWindow.xaml.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Identity.Client;
+
+namespace wpf_msal
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //Set the API Endpoint to Graph 'me' endpoint
+        string graphAPIEndpoint = "https://graph.microsoft.com/v1.0/me";
+
+        //Set the scope for API call to user.read
+        string[] scopes = new string[] { "user.read" };
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Call AcquireToken - to acquire a token requiring user to sign-in
+        /// </summary>
+        private async void CallGraphButton_Click(object sender, RoutedEventArgs e)
+        {
+            AuthenticationResult authResult = null;
+            var app = App.PublicClientApp;
+            ResultText.Text = string.Empty;
+            TokenInfoText.Text = string.Empty;
+
+            var accounts = await app.GetAccountsAsync();
+            var firstAccount = accounts.FirstOrDefault();
+
+            try
+            {
+                authResult = await app.AcquireTokenSilent(scopes, firstAccount)
+                    .ExecuteAsync();
+            }
+            catch (MsalUiRequiredException ex)
+            {
+                // A MsalUiRequiredException happened on AcquireTokenSilent.
+                // This indicates you need to call AcquireTokenInteractive to acquire a token
+                System.Diagnostics.Debug.WriteLine($"MsalUiRequiredException: {ex.Message}");
+
+                try
+                {
+                    authResult = await app.AcquireTokenInteractive(scopes)
+                        .WithAccount(accounts.FirstOrDefault())
+                        .WithPrompt(Prompt.SelectAccount)
+                        .ExecuteAsync();
+                }
+                catch (MsalException msalex)
+                {
+                    ResultText.Text = $"Error Acquiring Token:{System.Environment.NewLine}{msalex}";
+                }
+            }
+            catch (Exception ex)
+            {
+                ResultText.Text = $"Error Acquiring Token Silently:{System.Environment.NewLine}{ex}";
+                return;
+            }
+
+            if (authResult != null)
+            {
+                ResultText.Text = await GetHttpContentWithToken(graphAPIEndpoint, authResult.AccessToken);
+                DisplayBasicTokenInfo(authResult);
+                this.SignOutButton.Visibility = Visibility.Visible;
+            }
+        }
+
+        /// <summary>
+        /// Perform an HTTP GET request to a URL using an HTTP Authorization header
+        /// </summary>
+        /// <param name="url">The URL</param>
+        /// <param name="token">The token</param>
+        /// <returns>String containing the results of the GET operation</returns>
+        public async Task<string> GetHttpContentWithToken(string url, string token)
+        {
+            var httpClient = new System.Net.Http.HttpClient();
+            System.Net.Http.HttpResponseMessage response;
+            try
+            {
+                var request = new System.Net.Http.HttpRequestMessage(System.Net.Http.HttpMethod.Get, url);
+                //Add the token in Authorization header
+                request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+                response = await httpClient.SendAsync(request);
+                var content = await response.Content.ReadAsStringAsync();
+                return content;
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+        /// <summary>
+        /// Display basic information contained in the token
+        /// </summary>
+        private void DisplayBasicTokenInfo(AuthenticationResult authResult)
+        {
+            TokenInfoText.Text = "";
+            if (authResult != null)
+            {
+                TokenInfoText.Text += $"Username: {authResult.Account.Username}" + Environment.NewLine;
+                TokenInfoText.Text += $"Token Expires: {authResult.ExpiresOn.ToLocalTime()}" + Environment.NewLine;
+            }
+        }
+        /// <summary>
+        /// Sign out the current user
+        /// </summary>
+        private async void SignOutButton_Click(object sender, RoutedEventArgs e)
+        {
+            var accounts = await App.PublicClientApp.GetAccountsAsync();
+
+            if (accounts.Any())
+            {
+                try
+                {
+                    await App.PublicClientApp.RemoveAsync(accounts.FirstOrDefault());
+                    this.ResultText.Text = "User has signed-out";
+                    this.CallGraphButton.Visibility = Visibility.Visible;
+                    this.SignOutButton.Visibility = Visibility.Collapsed;
+                }
+                catch (MsalException ex)
+                {
+                    ResultText.Text = $"Error signing-out user: {ex.Message}";
+                }
+            }
+        }
+    }
+}
